72af83c0a16adb4fb7a35358a86ae153b234229b
Davis Kaza <156680027+dkazha@users.noreply.github.com>
2025-11-12 11:49:05 +0100

    Added two-loop solution, iteration 1

--- stock_picker.rb
+++ stock_picker.rb
@@ -1,14 +1,19 @@
 stock_prices = [17,3,6,9,15,8,6,1,10]
 
 def stock_picker(prices)
-  # Find the days that represent the best day to buy and the best day to sell
-  # First, see if the first value is the highest or the last value is the lowest
-  unless prices.max == prices[0] || prices.min == prices[prices.length - 1]
-#    prices.each_with_index.reduce(0) do |acc, (k,v)|
-#      if acc > k
-#        acc = k
-#        smallest = v
-#      end
-#    end
+  buy_day = 0
+  sell_day = 0
+  highest_profit = 0
+
+  prices.each_with_index do |price1, index1|
+    p "First loop: price1 #{price1} index1 #{index1}"
+    prices.each_with_index do |price2, index2|
+      p "Second loop: price2 #{price2} index2 #{index2}"
+
+    end
   end
+  puts "Best day to buy: #{buy_day}"
+  puts "Best day to sell: #{sell_day}"
 end
+
+stock_picker(stock_prices)
