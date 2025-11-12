6e994ab1dc92834db7036041fa4a14d3c43f19e1
Davis Kaza <156680027+dkazha@users.noreply.github.com>
2025-11-12 12:00:00 +0100

    Add checks
    This is to determine the highest profit. It needs to compare the current
    highest to the current profit. Then it sees if the current buy day is before
    the current sell day (we cant profit by buying in the future, we're not on Wall Street).

--- stock_picker.rb
+++ stock_picker.rb
@@ -5,15 +5,22 @@ def stock_picker(prices)
   sell_day = 0
   highest_profit = 0
 
-  # Loop over each price and index
+  # Loop over each price and index to find the buy day
   prices.each_with_index do |price1, index1|
     p "First loop: price1 #{price1} index1 #{index1}"
-    # Loop a second time in order to compare prices
+    # Loop a second time in order to compare prices and find the sell day
     prices.each_with_index do |price2, index2|
       p "Second loop: price2 #{price2} index2 #{index2}"
       # Check the profit
       profit = price2 - price1
       p "Profit: #{profit}"
+      # Check if the profit is the highest
+      # Check if the buy day came before the sell day
+      if profit > highest_profit && index1 < index2
+        highest_profit = profit
+        buy_day = index1
+        sell_day = index2
+      end
     end
   end
   puts "Best day to buy: #{buy_day}"
